40e7d87ff24430239c6a60d5ee5f6660ec12d38d
Facundo Fernandez <99399998+1991facundo@users.noreply.github.com>
2024-09-04 12:00:15 +0200

    reset test

---
+++ saturn-vanity/main.js
@@ -0,0 +1,22 @@
+// CARROUSEL DE IMAGENES CON LAS MARCAS
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Selecciona el div donde se va a cargar la sección de marcas
+    const brandsSection = document.getElementById('brands-section');
+  
+    // Cargar el contenido de brands.html dinámicamente
+    fetch('./components/brands.html')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Error al cargar el archivo brands.html');
+        }
+        return response.text();
+      })
+      .then(data => {
+        brandsSection.innerHTML = data;
+      })
+      .catch(error => {
+        console.error('Hubo un problema al cargar brands.html:', error);
+      });
+  });
+  
--- saturnVanity/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const carouselWrapper = document.getElementById('carousel-wrapper');
-    const carouselItems = Array.from(carouselWrapper.children);
-
-    // Clonar los elementos para un desplazamiento continuo
-    carouselItems.forEach(item => {
-        carouselWrapper.appendChild(item.cloneNode(true));
-    });
-});
