4a327a2768bfb0cabd27c9cbc094a9aab565781a
Facundo Fernandez <99399998+1991facundo@users.noreply.github.com>
2024-09-05 14:39:35 +0200

    animation ready

--- saturn-vanity/main.js
+++ saturn-vanity/main.js
@@ -1,14 +1,33 @@
-// ANIMACION
-
 import { Application } from '@splinetool/runtime';
 
 const canvas = document.getElementById('canvas3d'); 
+const mobileAnimationUrl = 'https://prod.spline.design/AUsDzex240CPllC9/scene.splinecode'; // URL para mobile
+const desktopAnimationUrl = 'https://prod.spline.design/35tJvHXnJye50ulQ/scene.splinecode'; // URL para desktop
+
 if (canvas) {
-  const app = new Application(canvas);
-  app.load('https://prod.spline.design/35tJvHXnJye50ulQ/scene.splinecode');
+    const app = new Application(canvas);
+    let currentAnimation = null; 
+
+    // Función para cargar la animación adecuada según el tamaño de la pantalla
+    function loadCanvasAnimation() {
+        if (window.innerWidth <= 450 && currentAnimation !== 'mobile') {
+            app.load(mobileAnimationUrl); // Cargar animación móvil
+            currentAnimation = 'mobile'; // Actualizar el estado actual
+        } else if (window.innerWidth > 450 && currentAnimation !== 'desktop') {
+            app.load(desktopAnimationUrl); // Cargar animación desktop
+            currentAnimation = 'desktop'; // Actualizar el estado actual
+        }
+    }
+
+    // Cargar la animación correcta al cargar la página
+    loadCanvasAnimation();
+
+    // Cargar la animación correcta solo si es necesario cuando la ventana cambie de tamaño
+    window.addEventListener('resize', loadCanvasAnimation);
 } else {
-  console.error("No se pudo encontrar el elemento canvas con el ID 'canvas3d'");
+    console.error("No se pudo encontrar el elemento canvas con el ID 'canvas3d'");
 }
+
 
 
 
